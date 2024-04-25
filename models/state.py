cd56294c357fc0f71f9846cef9a77ba3bcebd637
math198512 <thoraichi@gmail.com>
2024-04-25 09:30:24 -0700

    state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import models
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import environ
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
     name = ""
