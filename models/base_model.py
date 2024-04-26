d1a455ae363b66a0860d8817901e5353b2f7884d
math198512 <thoraichi@gmail.com>
2024-04-26 10:31:09 -0700

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -17,19 +17,15 @@ class BaseModel():
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        from models import storage
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
         if kwargs:
-            for key in kwargs:
-                if key == "__class__":
-                    continue
-                elif key in ("created_at", "updated_at"):
-                    iso = "%Y-%m-%dT%H:%M:%S.%f"
-                    setattr(self, key, datetime.strptime(kwargs[key], iso))
-                else:
-                    setattr(self, key, kwargs[key])
-                self.id = str(uuid4())
-        else:
-            self.id = str(uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self., key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
