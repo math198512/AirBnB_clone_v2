31e04babeccd24d71a1a2922733c7c78452cb0de
math198512 <thoraichi@gmail.com>
2024-04-26 12:31:20 -0700

    user.py

--- models/user.py
+++ models/user.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column
+from sqlalchemy import String
 
 
 class User(BaseModel, Base):
