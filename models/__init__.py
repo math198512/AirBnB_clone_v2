a81dd62e24c9b24089f6377f0675c5f03b0d9768
math198512 <thoraichi@gmail.com>
2024-04-26 07:44:52 -0700

    __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -24,8 +24,8 @@ storage_engine = getenv("HBNB_TYPE_STORAGE")
 if (storage_engine == "db"):
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
+    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-
-storage.reload()
+    storage.reload()
