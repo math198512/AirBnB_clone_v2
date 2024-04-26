a10edbd66210cd27bea2c9beae83be61377b6c35
math198512 <thoraichi@gmail.com>
2024-04-25 23:21:50 -0700

    city.py

--- models/city.py
+++ models/city.py
@@ -6,11 +6,15 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey
 from os import environ
 
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
+
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), nullable=False, ForeignKey(State.id))
-    state_id = ""
-    name = ""
+    if storage_engine == "db":
+        __tablename__ = "cities"
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), nullable=False, ForeignKey(State.id))
+    else:
+        state_id = ""
+        name = ""
