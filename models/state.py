44500ca8fe1029d0e73346202f3d434baad62985
math198512 <thoraichi@gmail.com>
2024-04-25 23:27:26 -0700

    state.py

--- models/state.py
+++ models/state.py
@@ -6,9 +6,23 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import environ
 
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
+
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    name = ""
+    if storage_engine == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+        @property
+        def cities(self):
+            """cities list
+            """
+            result = []
+            for j, i in models.storage.all(models.city.City).items():
+                if (i.state_id == self.id):
+                    result.append(i)
+            return result
