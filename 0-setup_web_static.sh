15725f5b0f06d6d64fa7903f5c40b7bb4269cec4
math198512 <thoraichi@gmail.com>
2024-04-28 01:50:47 -0700

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -22,6 +22,6 @@ sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 string_for_replacement="listen 80 default_server;\n\tlocation /hbnb_static { alias /data/web_static/current/;}"
-sudo sed -i "s/listen 80 default_server;/$string_for_replacement/" /etc/nginx/sites-enabled/default
+sudo sed -i "s.listen 80 default_server;.$string_for_replacement." /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
