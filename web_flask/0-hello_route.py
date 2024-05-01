4fe42d52998d64d308be47f59fad80c0e69b35e2
math198512 <thoraichi@gmail.com>
2024-04-30 23:55:21 -0700

    0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" starts a Flask web application """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ here """
+    return "Hello HBNB!"
