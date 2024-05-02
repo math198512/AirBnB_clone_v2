21572341dfd5c6e90bfe61856ed35ccc1345e820
math198512 <thoraichi@gmail.com>
2024-05-02 01:20:32 -0700

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,11 +23,13 @@ def ctext(text):
     """ here """
     return "C {}".format(text.replace('_', ' '))
 
+
 @app.route('/python/', defaults={'text': 'is_cool'})
 @app.route('/python/<text>', strict_slashes=False)
 def ptext(text):
     """ here """
     return "Python {}".format(text.replace('_', ' '))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
