c580079da683edf000438b2d6797804ec413f864
math198512 <thoraichi@gmail.com>
2024-05-02 01:15:04 -0700

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" starts a Flask web application """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ here """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ here """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def ctext(text):
+    """ here """
+    return "C {}".format(text.replace('_', ' '))
+
+@app.route("/python/<text>", strict_slashes=False)
+def ptext(text="is cool"):
+    """ here """
+    return "Python {}".format(text.replace('_', ' '))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
