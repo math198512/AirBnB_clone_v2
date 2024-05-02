7a9ed968c1d667ecb03362909ebc2366d014dd29
math198512 <thoraichi@gmail.com>
2024-05-02 05:52:46 -0700

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,7 +65,7 @@ class FileStorage:
             del FileStorage.__objects[obj_to_del]
         except AttributeError:
             pass
-    
+
     def close(self):
         """ close: to be added """
         self.reload()
